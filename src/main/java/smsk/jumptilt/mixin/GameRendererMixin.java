b52466cbdc1cfde1ee28a9af9c255bbc7a89b004
Smajslo <16209307+SmajloSlovakian@users.noreply.github.com>
2024-06-13 15:11:13 +0200

    clamp breaking added to the config

--- src/main/java/smsk/jumptilt/mixin/GameRendererMixin.java
+++ src/main/java/smsk/jumptilt/mixin/GameRendererMixin.java
@@ -16,20 +16,20 @@ import smsk.jumptilt.JT;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     private float tiltDegrees = 0;
-    private float clampingAmount = 0.1f;
 
     @Inject(method = "bobView",at = @At("TAIL"))
-    private void jumpTiltTest(MatrixStack matrices, float tickDelta, CallbackInfo ci){
+    private void tiltScreen(MatrixStack matrices, float tickDelta, CallbackInfo ci){
         try {
             float targetTilt = (float) JT.mc.player.getVelocity().y * -Config.cfg.amount;
 
-            targetTilt = customClamp(JT.mc.player.prevPitch + targetTilt, -90 - Config.cfg.upperClamping, 90 + Config.cfg.lowerClamping, clampingAmount, clampingAmount) - JT.mc.player.prevPitch;
+            targetTilt = customClamp(JT.mc.player.prevPitch + targetTilt, -90 - Config.cfg.upperClamping, 90 + Config.cfg.lowerClamping, Config.cfg.lowerClampBreak, Config.cfg.upperClampBreak) - JT.mc.player.prevPitch;
             if (JT.mc.player.isOnGround()) targetTilt = 0;
             
             tiltDegrees = MathHelper.clamp((tiltDegrees - targetTilt) * (float) Math.pow(Config.cfg.speed, JT.mc.getRenderTickCounter().getLastFrameDuration()) + targetTilt, -90, 90);
             matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(tiltDegrees));
         } catch (Exception e) {}
     }
+
     private float customClamp(float val, float min, float max, float amountForMin, float amountForMax) {
         if (val < min) {
             val = (val - min) * amountForMin + min;
