ccc1d4bfa6ea5b38243f77f46adcb0d8ba11e0a5
Smajslo <16209307+SmajloSlovakian@users.noreply.github.com>
2024-06-13 15:00:13 +0200

    update to 1.21

--- src/main/java/smsk/jumptilt/mixin/GameRendererMixin.java
+++ src/main/java/smsk/jumptilt/mixin/GameRendererMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
@@ -15,24 +16,19 @@ import smsk.jumptilt.JT;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     private float tiltDegrees = 0;
-
     private float clampingAmount = 0.1f;
 
-    @Inject(method = "render",at = @At("HEAD"))
-    private void frame(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+    @Inject(method = "bobView",at = @At("TAIL"))
+    private void jumpTiltTest(MatrixStack matrices, float tickDelta, CallbackInfo ci){
         try {
             float targetTilt = (float) JT.mc.player.getVelocity().y * -Config.cfg.amount;
 
             targetTilt = customClamp(JT.mc.player.prevPitch + targetTilt, -90 - Config.cfg.upperClamping, 90 + Config.cfg.lowerClamping, clampingAmount, clampingAmount) - JT.mc.player.prevPitch;
             if (JT.mc.player.isOnGround()) targetTilt = 0;
             
-            tiltDegrees = MathHelper.clamp((tiltDegrees - targetTilt) * (float) Math.pow(Config.cfg.speed, JT.mc.getLastFrameDuration()) + targetTilt, -90, 90);
+            tiltDegrees = MathHelper.clamp((tiltDegrees - targetTilt) * (float) Math.pow(Config.cfg.speed, JT.mc.getRenderTickCounter().getLastFrameDuration()) + targetTilt, -90, 90);
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(tiltDegrees));
         } catch (Exception e) {}
-    }
-
-    @Inject(method = "bobView",at = @At("TAIL"))
-    private void jumpTiltTest(MatrixStack matrices, float tickDelta, CallbackInfo ci){
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(tiltDegrees));
     }
     private float customClamp(float val, float min, float max, float amountForMin, float amountForMax) {
         if (val < min) {
